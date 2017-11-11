aab46259be5947f917b0bb2401b46e4601516cba
bethtechow <bethtechow@yahoo.com>
2017-11-11 13:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
-const currentUser = `Beth is awesome`;
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!'';
+const currentUser = "Beth is awesome";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
