f5b9b08db5dfeb2f8eb9e96d49969487354f7ca6
bethtechow <bethtechow@yahoo.com>
2017-11-11 13:20:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 const currentUser = "Beth is awesome";
 const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+const welcomeMessage = welcomeMessage.toUpperCase();
